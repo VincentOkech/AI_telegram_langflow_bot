22cb2ae85e7d8e1882bf9bdeb6ec90a152455b5a
Vinn <65304570+VincentOkech@users.noreply.github.com>
2025-02-16 09:32:38 +0300

    Update bot3.py

--- bot3.py
+++ bot3.py
@@ -1,4 +1,4 @@
-TELEGRAM_TOKEN = "7867905932:AAFAGA-2SSZ4qRxlzo7p-AprhhIfJSjZ548"
+TELEGRAM_TOKEN = "7867905932:AAFAGA-2SSp-AprhhIfJSjZ548"
 import requests
 url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getMe"
 response = requests.get(url)
